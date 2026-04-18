f7ba648397bc341893a46b17811fb0e875848c64
sokkhenkheum7-lang <sokkhenkheum7@gmail.com>
2026-04-18 15:59:50 +0700

    combin.

---
+++ Review_section/combin.py
@@ -0,0 +1,71 @@
+# name = "Khen"
+# passwords = "168233"
+# address = "phnom Penh"
+
+# s = input("Enter you id:")
+
+# print ("---------------your information-----------")
+
+# if s == passwords:
+#     print ("can continues",)
+#     print ("Enter your name:",name)
+#     print ("Enter your address:",address)
+# else:
+#     print ("your info have error")
+    
+# i = 0 
+# for i in range (3):
+#     print ("Hello")
+
+# i = 0
+# if i < 23:
+#     i += 1
+#     print ("welcome")
+    
+    
+# i = 1 
+# while i < 6:
+#     print ("wello",i)
+#     print (i)
+#     i+=1
+
+# user_id = True 
+# while user_id:
+#     r = input("Enter user :")
+#     if r == "yes":
+#         user_id = False
+        
+#         print ("You have completed")
+
+# user_id = True 
+
+# while user_id:
+#     r = input("Enter user (yes/no): ")
+
+#     if r == "yes":
+#         print("You have completed")
+#         user_id = False
+
+#     elif r == "no":
+#         print("Try again...")
+
+#     else:
+#         print("Invalid input, please enter yes or no")
+
+# tup = (1,2,3,4)
+# 3 in tup 
+# print (tup)
+
+# list_country = ["Cambodia","Malaysia","China","Korean","Vietnam"]
+# print ("which one you love ? ",list_country[len(list_country)-1])
+
+while True:
+    r = input("Enter user (yes/no): ")
+
+    if r == "yes":
+        print("You have completed")
+        break
+    elif r == "no":
+        print("Try again...")
+    else:
+        print("Invalid input")
